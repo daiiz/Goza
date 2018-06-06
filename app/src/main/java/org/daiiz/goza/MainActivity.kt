b81d773cf7b8811fbcad455738d745194a0fa89e
daiiz <da.iz.mg@gmail.com>
2018-06-07 01:21:27 +0900

    Logの書き方学んだ

--- app/src/main/java/org/daiiz/goza/MainActivity.kt
+++ app/src/main/java/org/daiiz/goza/MainActivity.kt
@@ -2,11 +2,13 @@ package org.daiiz.goza
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.v("foo", "123")
         setContentView(R.layout.activity_main)
     }
 }
