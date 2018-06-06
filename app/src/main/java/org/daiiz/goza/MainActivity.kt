c984f2ebc73d50fc11c3e58c13016c693204e227
daiiz <da.iz.mg@gmail.com>
2018-06-07 01:43:16 +0900

    webview的にはこれでいい でもwebviewを使うのはだめだった

--- app/src/main/java/org/daiiz/goza/MainActivity.kt
+++ app/src/main/java/org/daiiz/goza/MainActivity.kt
@@ -3,12 +3,26 @@ package org.daiiz.goza
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.v("foo", "123")
         setContentView(R.layout.activity_main)
+        renderScrapbox()
+    }
+
+    fun renderScrapbox () {
+        lateinit var webview : WebView
+        webview = findViewById(R.id.main_webview)
+        webview.webViewClient = object: WebViewClient () {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                return false
+            }
+        }
+        webview.loadUrl("https://scrapbox.io/")
     }
 }
